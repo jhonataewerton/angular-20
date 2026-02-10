9e500cdad51668481602976e5ad450c89481b0d9
Jhonata Ewerton Marcos <jhonata.as@hotmail.com>
2026-02-10 14:50:10 -0300

    Initial commit - Angular project

---
+++ src/app/core/layout/layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Header } from './components/header/header';
+import { ɵEmptyOutletComponent } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  imports: [Header],
+  templateUrl: './layout.html',
+  styleUrl: './layout.scss',
+})
+export class Layout {}
