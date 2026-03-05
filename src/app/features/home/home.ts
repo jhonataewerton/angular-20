b7fd839b643be2487f992d6279cc933d46c9b625
Jhonata Ewerton Marcos <jhonata.as@hotmail.com>
2026-03-05 17:05:24 -0300

    feat: add service agnostic to open dialog confirmation and add action to remove a transaction

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,4 +1,5 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { FeedbackService } from './../../shared/feddback/services/feedback.service';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
 import { Balance } from './components/balance/balance';
 import { TransactionItem } from './components/transaction-item/transaction-item';
 import { Transaction } from '../../shared/transaction/interfaces/transaction';
@@ -6,6 +7,8 @@ import { NoTransactions } from './components/no-transactions/no-transactions';
 import { TransactionsService } from '../../shared/transaction/services/transactions.service';
 import { MatButtonModule } from '@angular/material/button';
 import { Router, RouterLink } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmationDialogService } from '../../../shared/dialog/confirmation/services/confirmation-dialog.service';
 
 @Component({
   selector: 'app-home',
@@ -15,8 +18,9 @@ import { Router, RouterLink } from '@angular/router';
 })
 export class Home implements OnInit {
   private transactionsService = inject(TransactionsService);
-
+  private feedbackService = inject(FeedbackService);
   private router = inject(Router);
+  private confirmationDialogService = inject(ConfirmationDialogService);
 
   transactions = signal<Transaction[]>([]);
 
@@ -24,8 +28,34 @@ export class Home implements OnInit {
     this.getTransections();
   }
 
-  edit($event: Transaction) {
-    this.router.navigate(['edit', $event.id]);
+  edit($transaction: Transaction) {
+    this.router.navigate(['edit', $transaction.id]);
+  }
+
+  remove(transaction: Transaction) {
+    this.confirmationDialogService
+      .open({
+        title: 'Deletar transação',
+        message: 'Você realmente quer deletar a transação?',
+        yesButtonText: 'Sim',
+        noButtonText: 'Não',
+      })
+      .subscribe({
+        next: () => {
+          this.removeTransactionFromArray(transaction);
+          this.feedbackService.success('Transação removida com sucesso');
+        },
+      });
+
+    this.transactionsService.delete(transaction.id).subscribe({
+      next: () => {},
+    });
+  }
+
+  private removeTransactionFromArray(transaction: Transaction) {
+    this.transactions.update((transactions) =>
+      transactions.filter((item) => item.id !== transaction.id),
+    );
   }
 
   private getTransections() {
