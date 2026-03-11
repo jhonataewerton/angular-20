ea133dedc01cc0964cd1d210bf8e67c6aecbf544
Jhonata Ewerton Marcos <jhonata.as@hotmail.com>
2026-03-11 18:19:37 -0300

    chore: add path mapping.

--- src/app/features/home/pages/create-or-edit/resolvers/get-transaction-by-id-resolver.ts
+++ src/app/features/home/pages/create-or-edit/resolvers/get-transaction-by-id-resolver.ts
@@ -1,7 +1,7 @@
 import { ResolveFn } from '@angular/router';
-import { Transaction } from '../../../../../shared/transaction/interfaces/transaction';
-import { TransactionsService } from '../../../../../shared/transaction/services/transactions.service';
 import { inject } from '@angular/core';
+import { Transaction } from '@shared/transaction/interfaces/transaction';
+import { TransactionsService } from '@shared/transaction/services/transactions.service';
 
 export const getTransactionByIdResolver: ResolveFn<Transaction> = (route, state) => {
   const transactionsService = inject(TransactionsService);
--- src/app/features/home/pages/list/list.component.ts
+++ src/app/features/home/pages/list/list.component.ts
@@ -1,14 +1,14 @@
 import { Component, inject, input, linkedSignal } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-import { ConfirmationDialogService } from '../../../../../shared/dialog/confirmation/services/confirmation-dialog.service';
-import { FeedbackService } from '../../../../shared/feddback/services/feedback.service';
-import { Transaction } from '../../../../shared/transaction/interfaces/transaction';
-import { TransactionsService } from '../../../../shared/transaction/services/transactions.service';
 import { MatButtonModule } from '@angular/material/button';
 import { Balance } from './components/balance/balance';
 import { NoTransactions } from './components/no-transactions/no-transactions';
 import { TransactionItem } from './components/transaction-item/transaction-item';
 import { TransactionsContainerComponent } from './components/transactions-container/transactions-container.component';
+import { FeedbackService } from '@shared/feddback/services/feedback.service';
+import { Transaction } from '@shared/transaction/interfaces/transaction';
+import { TransactionsService } from '@shared/transaction/services/transactions.service';
+import { ConfirmationDialogService } from '../../../../../shared/dialog/confirmation/services/confirmation-dialog.service';
 
 @Component({
   selector: 'app-list',
